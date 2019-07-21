5693192bcd3cc0c4821942d8036ad2a117f8a6d8
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-07-21 14:48:54 -0500

    Create _app.js

---
+++ src/pages/_app.js
@@ -0,0 +1,61 @@
+import React from "react";
+import App, { Container } from "next/app";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../components/theme";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    if (ctx.req && ctx.req.session.passport) {
+      pageProps.user = ctx.req.session.passport.user;
+    }
+    return { pageProps };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: props.pageProps.user
+    };
+  }
+
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      // jssStyles.parentNode!.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    const props = {
+      ...pageProps,
+      user: this.state.user
+    };
+
+    return (
+      <Container>
+        <Head>
+          <title>
+            Experience Trail - Create Connections Through Shared Travel
+            Experiences
+          </title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Container>
+    );
+  }
+}
+
+export default MyApp;
