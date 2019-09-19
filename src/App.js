dd6eff4ec29c555db3ebfa1a5a793dba57fe90b6
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-09-19 12:22:05 -0500

    Revert "Merge pull request #6 from experience-trail/reactstrap-ui-kit"
    This reverts commit ea5920a2c4612c912659b4dd5a146742f0f0b6ad, reversing
    changes made to 547a92092d2467228338d3faacae83893bcb5166.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import { Container } from "reactstrap";
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Container } from 'reactstrap'
 
 import PrivateRoute from './components/PrivateRoute'
 import Loading from './components/Loading'
@@ -16,17 +11,7 @@ import Profile from './views/Profile'
 import { useAuth0 } from './react-auth0-spa'
 
 // styles
-import "assets/css/bootstrap.min.css";
-import "assets/scss/now-ui-kit.scss";
-import "assets/demo/demo.css";
-import "assets/demo/nucleo-icons-page-styles.css";
-
-// demo pages
-import Index from "views/Index.js";
-import NucleoIcons from "views/NucleoIcons.js";
-import LoginPage from "views/examples/LoginPage.js";
-import LandingPage from "views/examples/LandingPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
+import './App.css'
 
 // fontawesome
 import initFontAwesome from './utils/initFontAwesome'
@@ -40,38 +25,21 @@ const App = () => {
 		return <Loading />
 	}
 
-  return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <PrivateRoute path="/profile" component={Profile} />
-
-        {/* Test pages */}
-        <Route path="/test" component={Test} />
-
-        {/* Demo pages */}
-        <Route path="/components-demo" render={props => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons-demo"
-          render={props => <NucleoIcons {...props} />}
-        />
-        <Route
-          path="/landing-page-demo"
-          render={props => <LandingPage {...props} />}
-        />
-        <Route
-          path="/profile-page-demo"
-          render={props => <ProfilePage {...props} />}
-        />
-        <Route
-          path="/login-page-demo"
-          render={props => <LoginPage {...props} />}
-        />
-      </Switch>
-      <Footer />
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<div id="app" className="d-flex flex-column h-100">
+				<NavBar />
+				<Container className="flex-grow-1 mt-5">
+					<Switch>
+						<Route path="/" exact component={Home} />
+						<Route path="/test" component={Test} />
+						<PrivateRoute path="/profile" component={Profile} />
+					</Switch>
+				</Container>
+				<Footer />
+			</div>
+		</Router>
+	)
+}
 
 export default App
