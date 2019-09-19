1053b3a7f0d2779d0c32989c7f65b982410c250b
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2019-09-19 11:36:53 -0500

    Revert "Apollo Hooks on Auth"

--- src/index.js
+++ src/index.js
@@ -15,53 +15,44 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {ApolloProvider} from '@apollo/react-hooks';
-import {ApolloClient} from 'apollo-client';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-import {HttpLink} from 'apollo-link-http';
-import {Auth0Provider} from './react-auth0-spa';
-import config from './auth_config.json';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Auth0Provider } from "./react-auth0-spa";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 
-const cache = new InMemoryCache();
-const link = new HttpLink({
-    uri: 'https://d3vr82ig632z51.cloudfront.net/graphql',
-});
+import config from "./auth_config.json";
+
 const client = new ApolloClient({
-    cache,
-    link,
+  uri: 'https://d3vr82ig632z51.cloudfront.net/graphql',
+  resolvers: {},
 });
-
-// const client = new ApolloClient({
-//     uri: 'https://d3vr82ig632z51.cloudfront.net/graphql',
-//     resolvers: {},
-// });
 
 const onRedirectCallback = appState => {
-    window.history.replaceState(
-        {},
-        document.title,
-        appState && appState.targetUrl
-            ? appState.targetUrl
-            : window.location.pathname,
-    );
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
 };
+  
 
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <Auth0Provider
-            domain={config.domain}
-            client_id={config.clientId}
-            redirect_uri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-        >
-            <App />
-        </Auth0Provider>
+  <ApolloProvider client={client}>
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+    >
+    <App />
+  </Auth0Provider>
     </ApolloProvider>,
-    document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
